38dbb1dbc5857e07014e62aab58f85f882f2a304
Natanbrg <natan.nt2@hotmail.com>
2024-09-18 16:50:57 -0300

    update files

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/Natanbrg/Portfolio/refs/heads/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
